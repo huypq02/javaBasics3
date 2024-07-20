696f43a4422a2d67a424eac082adee0f0cd03aa5
Huy Pham <huypham0297@gmail.com>
2024-07-20 13:14:37 +0700

    chore: fix something bai3 and bai3_while in bai5_2

--- src/main/java/vn/edu/cybersoft/buoi5_2/bai3.java
+++ src/main/java/vn/edu/cybersoft/buoi5_2/bai3.java
@@ -2,8 +2,6 @@ package vn.edu.cybersoft.buoi5_2;
 
 import vn.edu.cybersoft.pkg.utils;
 
-import java.util.Scanner;
-
 public class bai3 {
     final static int MAX_NUM = 1000;
     final static int DIVIDEND = 3;
@@ -11,16 +9,16 @@ public class bai3 {
         try {
             // co bao nhieu so chia het cho 3 tu 0 den 1000?
             //
-            System.out.printf("Result is %d\n", totalNumberIsDivisible(MAX_NUM));
+            System.out.printf("Result is %d\n", numberOfNumbersIsDivisible());
 
         } catch (Exception e){
             System.out.println("Something went wrong!");
         }
     }
-
-    private static int totalNumberIsDivisible(int n){
+    //
+    private static int numberOfNumbersIsDivisible(){
         int sum = 0;
-        for (int i = 0; i <= n; i++){
+        for (int i = 0; i <= MAX_NUM; i++){
             if (utils.isDivisible(i, DIVIDEND)) sum++;
         }
         return sum;
